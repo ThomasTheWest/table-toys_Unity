d476d3107138449a6de5eeb36eacfb14ccf1f9bf
Westprog <JulianHWWest@gmail.com>
2025-10-08 16:02:31 +0100

    Optimisations
    Made it work + smaller

--- Assets/Scripts/fxAbacus.cs
+++ Assets/Scripts/fxAbacus.cs
@@ -13,98 +13,69 @@ public class fxAbacus : MonoBehaviour
     [SerializeField] private Transform[] posBackward;
 
     //indexForwards represents which position is next to move to. 0 also corresponds with the first bead, 1 the second ect.
-    public int indexForward, indexBackward;
+    //indexBackwards works similarly but counting inwards from 0
+    //indexMax is defined by the total number of beads. (Length of the array -1)
+    //beadsRight keeps track of how many beads are on the right
+    public int indexForward;
     private int indexMax;
-    private int beadsRight;
 
-    private bool isSliding = false;
-    [SerializeField] private float slideDuration = 2.0f;
+    private bool isSliding = false; // Defined to be true when we're in the routine to make the beads slide - used to end the coroutine
+    [SerializeField] private float slideDuration = 2.0f; // ### What is the unit of slide duration? What is the meaning?
     void Start()
     {
         indexMax = beads.Length - 1;
-        indexBackward = 0;
         indexForward = 0;
-        beadsRight = 0;
     }
 
     void Update()
     {
 
     }
-    private IEnumerator SlideRight()
+    
+    private IEnumerator Slide(bool ToRight) 
     {
-        if (indexForward == indexMax && beadsRight == beads.Length) yield break;
-        
+        // Check whether there are any beads remaining to move. If not, just return
+        if (ToRight && indexForward > indexMax ) yield break;
+        if (!ToRight && indexForward <= 0 ) yield break;
+
+        // There is a bead to be moved, so we tell the coroutine to keep returning until we've moved it
         isSliding = true;
-        
+
         float elapsed = 0f;
-        
+
         while (elapsed <= slideDuration)
         {
-            beads[indexForward].transform.position = Vector3.Lerp(beads[indexForward].transform.position, posForward[indexForward].position, slideDuration);
+            if (ToRight) 
+                beads[indexForward].transform.position = Vector3.Lerp(beads[indexForward].transform.position, posForward[indexForward].position, slideDuration);
+            else
+                beads[indexForward-1].transform.position = Vector3.Lerp(beads[indexForward-1].transform.position, posBackward[indexForward-1].position, slideDuration);
+
+
             elapsed += Time.deltaTime;
             yield return null;
         }
-
-        if (indexForward >= 1)
-        {
-            indexBackward++;
-        }
-
-        if (indexForward != indexMax)
+            
+        if (ToRight)
         {
             indexForward++;
         }
-
-        beadsRight++;
-
-        isSliding = false;
-
-        Debug.Log("indexForward: " + indexForward + " indexBackward: " + indexBackward + " Beads on Right: " + beadsRight);
-    }
-
-    private IEnumerator SlideLeft()
-    {
-        if (indexForward == 0 && beadsRight > 0) yield break;
-
-        isSliding = true;
-
-        float elapsed = 0f;
-
-        while (elapsed <= slideDuration)
-        {
-            if (beadsRight == beads.Length && indexForward == indexMax)
-            {
-                beads[indexForward].transform.position = Vector3.Lerp(beads[indexForward].transform.position, posBackward[indexBackward].position, slideDuration);
-            }
-            else
-            {
-                beads[indexForward - 1].transform.position = Vector3.Lerp(beads[indexForward - 1].transform.position, posBackward[indexBackward].position, slideDuration);
-            }
-
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        beadsRight--;
-
-        if (beadsRight != beads.Length || beadsRight != beads.Length - 1)
+        else
         {
             indexForward--;
         }
-        
-        indexBackward--;
+
 
         isSliding = false;
 
-        Debug.Log("indexForward: " + indexForward + " New indexBackward: " + indexBackward + " Beads on Right: " + beadsRight);
+        Debug.Log("indexForward: " + indexForward);
+
     }
 
     public void StartSlideRight()
     {
-        if (indexForward <= indexMax && !isSliding && beadsRight != beads.Length)
+        if (indexForward <= indexMax && !isSliding )
         {
-            StartCoroutine(SlideRight());
+            StartCoroutine(Slide(true));
         }
         else
         {
@@ -116,7 +87,7 @@ public class fxAbacus : MonoBehaviour
     {
         if (indexForward > 0 && !isSliding)
         {
-            StartCoroutine(SlideLeft());
+            StartCoroutine(Slide(false));
         }
         else
         {
