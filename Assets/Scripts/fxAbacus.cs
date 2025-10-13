7220989c33e9045d6587688cba3e352e8b509831
ThomasTheWest <tomtomtomadwwest@gmail.com>
2025-10-13 13:44:15 +0100

    Changed mats

--- Assets/Scripts/fxAbacus.cs
+++ Assets/Scripts/fxAbacus.cs
@@ -13,23 +13,16 @@ public class fxAbacus : MonoBehaviour
     [SerializeField] private Transform[] posBackward;
 
     //indexForwards represents which position is next to move to. 0 also corresponds with the first bead, 1 the second ect.
-    //indexBackwards works similarly but counting inwards from 0
     //indexMax is defined by the total number of beads. (Length of the array -1)
-    //beadsRight keeps track of how many beads are on the right
     public int indexForward;
     private int indexMax;
 
     private bool isSliding = false; // Defined to be true when we're in the routine to make the beads slide - used to end the coroutine
-    [SerializeField] private float slideDuration = 2.0f; // ### What is the unit of slide duration? What is the meaning?
+    [SerializeField] private float slideDuration = 0.5f; // Not a duration, more of a value. 
     void Start()
     {
         indexMax = beads.Length - 1;
         indexForward = 0;
-    }
-
-    void Update()
-    {
-
     }
     
     private IEnumerator Slide(bool ToRight) 
@@ -38,7 +31,7 @@ public class fxAbacus : MonoBehaviour
         if (ToRight && indexForward > indexMax ) yield break;
         if (!ToRight && indexForward <= 0 ) yield break;
 
-        // There is a bead to be moved, so we tell the coroutine to keep returning until we've moved it
+        // isSliding makes sure the bead has finished moving before allowing any other beads to move.
         isSliding = true;
 
         float elapsed = 0f;
@@ -49,7 +42,6 @@ public class fxAbacus : MonoBehaviour
                 beads[indexForward].transform.position = Vector3.Lerp(beads[indexForward].transform.position, posForward[indexForward].position, slideDuration);
             else
                 beads[indexForward-1].transform.position = Vector3.Lerp(beads[indexForward-1].transform.position, posBackward[indexForward-1].position, slideDuration);
-
 
             elapsed += Time.deltaTime;
             yield return null;
@@ -64,11 +56,9 @@ public class fxAbacus : MonoBehaviour
             indexForward--;
         }
 
-
         isSliding = false;
 
         Debug.Log("indexForward: " + indexForward);
-
     }
 
     public void StartSlideRight()
