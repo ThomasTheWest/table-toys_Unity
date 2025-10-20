1e414b53e6dc4069db035afb550865919c13f7c2
ThomasTheWest <tomtomtomadwwest@gmail.com>
2025-10-20 10:21:26 +0100

    Completed cosmetic test scene

--- Assets/Scripts/fxAbacus_mult.cs
+++ Assets/Scripts/fxAbacus_mult.cs
@@ -9,71 +9,62 @@ using TMPro;
 
 public class fxAbacus_mult : MonoBehaviour
 {
+    [Header("Arrays")]
     [SerializeField] private GameObject[] beads;
     [SerializeField] private Transform[] posForward;
     [SerializeField] private Transform[] posBackward;
-
-    //indexForwards represents which position is next to move to. 0 also corresponds with the first bead, 1 the second ect.
-    //indexMax is defined by the total number of beads. (Length of the array -1)
-    [HideInInspector] public int indexForward = 0;
-    private int indexMax;
+    
+    [HideInInspector] public int indexForward = 0; // indexForwards represents which position is next to move to. 0 also corresponds with the first bead, 1 the second ect.
+    private int indexMax; // indexMax is defined by the total number of beads. (Length of the array -1).  Set in Start()
 
     [Header("UI")]
-    [SerializeField] private string quality; //the name of the quality the field is used for (ie. Bravery, Charm, Wit)
-    [SerializeField] private TMP_Text valueDisplay;
-    [SerializeField] private TMP_InputField valueInput;
-    [SerializeField] private Button buttonConfirm;
-    private string qualityInput; // Quality Dice value, retrieved from numberInput
+    [SerializeField] private string quality; // Name of the quality the field is used for (ie. Bravery, Charm, Wit). Manually set in editor
+    [SerializeField] private TMP_Text valueDisplay; // Preview text in the input field, shows the amount of beads on the right
+    [SerializeField] private TMP_InputField valueInput; // Where the user punches in their desired amount of beads
 
     private int oldValue = 0; // This value is compared to for any changes in qualityInput. Once the beads have moved, oldInput is set to qualityInput.
     private int newValue = 0; // This is an int converted from the string from numberInput.text
 
     //private bool isSliding = false; // Defined to be true when we're in the routine to make the beads slide - used to end the coroutine
-    //[SerializeField] private float slideDuration = 0.5f; // Not a duration, an interpolation value for a transform lerp. 
+    //Don't really need it anymore tbh
+    //[SerializeField] private float slideDuration = 0.5f; // Not a duration, an interpolation value for a vector3.lerp. 
     void Start()
     {
         indexMax = beads.Length - 1;
     }
 
     private void Update()
-    {
+    {// This just checks for changes in user-inputted variables that were parsed by ConfirmValue()
+
         if (oldValue != newValue)
         {
             if (newValue < oldValue) //if the new input is lower than the current one, slide beads to the left
-            {
                 StartSlideLeft();
-                oldValue = newValue;
-            }
             else if (newValue > oldValue) //if the old value is lower than the new input, slide beads to the right
-            {
                 StartSlideRight();
-                oldValue = newValue;
-            }
+
+            oldValue = newValue;
         }
        
         if (oldValue != 0)
-        {
             valueDisplay.text = newValue.ToString();
-        }
         else
-        {
             valueDisplay.text = quality;
-        }
     }
 
     public void ConfirmValue()
-    {
+    {// Called whenever the button in-scene is pressed.
+
         if (valueInput.text == "true")
-        {
-            newValue = 10;
-        }
+            newValue = 10; //this is just a goof, don't worry about this
+        else if (valueInput.text == "")
+            return;
         else
-        {
-            int.TryParse(valueInput.text, out newValue);
-        }
+            int.TryParse(valueInput.text, out newValue); //takes string and gives int for Slide() to use
 
         valueInput.text = "";
-        //Debug.Log (newValue);
+
+        // add some code to clamp values to 1 - beads.Length
     }
 
     private IEnumerator Slide(bool ToRight, int toMove) 
@@ -85,32 +76,30 @@ public class fxAbacus_mult : MonoBehaviour
         // isSliding makes sure the bead has finished moving before allowing any other beads to move.
         //isSliding = true;
 
-        float elapsed = 0f;
+        //float elapsed = 0f;
 
         for (int i = 0; i < toMove; i++) //the transform while loop is done as many times as toMove is set (which is determined by either adding or subtracting from oldValue
         {
-            //while (elapsed <= slideDuration) //this while logic was written when I assumed the interpolation value for the lerp was a duration. it doesn't work like that
-            //{ taking out this while loop since vector3.lerp doesn't work like I thought it did
-            if (ToRight)
-                //beads[indexForward].transform.position = Vector3.Lerp(beads[indexForward].transform.position, posForward[indexForward].position, slideDuration);
-                //vector3.Lerp does not do what I think it does, just going to do a straight transform change
-                beads[indexForward].transform.position = posForward[indexForward].position;
-            else
-                //beads[indexForward - 1].transform.position = Vector3.Lerp(beads[indexForward - 1].transform.position, posBackward[indexForward - 1].position, slideDuration);
-                beads[indexForward - 1].transform.position = posBackward[indexForward - 1].position;
+            /*while (elapsed < slideDuration) //this while logic was written when I assumed the interpolation value for the lerp was a duration. it doesn't work like that
+            {
+                if (ToRight)
+                    beads[indexForward].transform.position = Vector3.Lerp(beads[indexForward].transform.position, posForward[indexForward].position, elapsed / slideDuration);
+                else
+                    beads[indexForward - 1].transform.position = Vector3.Lerp(beads[indexForward - 1].transform.position, posBackward[indexForward - 1].position, elapsed / slideDuration);
 
                 elapsed += Time.deltaTime;
                 yield return null;
-            //}
+            }*/
 
             if (ToRight)
-            {
-                indexForward++;
-            }
+                beads[indexForward].transform.position = posForward[indexForward].position;
             else
-            {
+                beads[indexForward - 1].transform.position = posBackward[indexForward - 1].position;
+
+            if (ToRight)
+                indexForward++;
+            else
                 indexForward--;
-            }
 
             //isSliding = false;
 
@@ -122,7 +111,7 @@ public class fxAbacus_mult : MonoBehaviour
     {
         if (indexForward <= indexMax)
         {
-            StartCoroutine(Slide(true, newValue - oldValue));
+            StartCoroutine(Slide(true, newValue - oldValue)); //if you need to move stuff to the right, you need the difference of what is already on the right to decide what to move
         }
         else
         {
@@ -134,7 +123,7 @@ public class fxAbacus_mult : MonoBehaviour
     {
         if (indexForward > 0)
         {
-            StartCoroutine(Slide(false, oldValue - newValue));
+            StartCoroutine(Slide(false, oldValue - newValue)); //if you need to move stuff to the left, you need the difference of what is already on the left to decide what to move
         }
         else
         {
